5b094969aed2926ec2e44d4d6f0d73286249ad96
rinku-parmar <rinkparmar022@gmail.com>
2025-01-15 17:54:59 +0530

    Improved layout and design of the listings page

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose=require("mongoose");
 const Listing =require("./models/listing");
 const path =require("path");
 const methodOverride = require('method-override');
+const ejsMate=require("ejs-mate")
+
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
 main().then(()=>{
@@ -15,11 +17,14 @@ async function main() {
   }
 
 
- app.set("view engine");
+  app.set("view engine", "ejs");
  app.set("views",path.join(__dirname,"views"));
  app.use(express.urlencoded({extended:true}))
  app.use(methodOverride('_method'));
- 
+ app.engine('ejs', ejsMate);
+ app.use(express.static(path.join(__dirname,"/public")))
+
+
 app.get("/",(req,res)=>{
     res.send("hi i am root");
 })
