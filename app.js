3511e799d258fb98ec02141654ace8c85e94cb7c
rinku-parmar <rinkparmar022@gmail.com>
2025-01-26 16:56:31 +0530

    Add ExpressError-To add error handling for routes that do not exist and to wrap all your routes with the wrapAsync function

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const Listing =require("./models/listing");
 const path =require("path");
 const methodOverride = require('method-override');
 const ejsMate=require("ejs-mate")
-const wrapAsync=require("./utils/wrapAsync.js")
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+
+
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -31,11 +34,11 @@ app.get("/",(req,res)=>{
 })
 
 //-------1-index route :show all
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     const allListings= await Listing.find({})
     //.then(res=>{ console.log(res) })
     res.render("listings/index.ejs",{allListings})
-})
+}))
 
 // -----------3---new Route
 app.get("/listings/new",  (req, res) => {
@@ -43,14 +46,14 @@ app.get("/listings/new",  (req, res) => {
 });
 
 // --------2-show route-  indival information
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing})
     // console.log(id);
-})
+}))
 
-//--4.crate route
+//--4.create route
 // app.post("/listings",async (req,res)=>{
 // //   let {title,description,image,price,country,location}=req.body
 //     // let listing=req.body.listing; //js obj
@@ -62,7 +65,7 @@ app.get("/listings/:id",async(req,res)=>{
 //    res.redirect("/listings")
 // }) 
 app.post("/listings", wrapAsync(async (req,res,next)=>{
-    
+       
         const newListing= new Listing(req.body.listing);
          await newListing.save();
           res.redirect("/listings")
@@ -72,37 +75,44 @@ app.post("/listings", wrapAsync(async (req,res,next)=>{
     
 
 //edit
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
        res.render("listings/edit.ejs",{listing})
-})
+}))
 
 //update route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
    const result= await Listing.findByIdAndUpdate(id,{...req.body.listing})
   res.redirect(`/listings/${id}`) //show route
 //  console.log(result);
-})
+}))
 
 //DELETE route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
    let {id}=req.params;
    let deletedListing= await Listing.findByIdAndDelete(id);
    console.log(deletedListing);
    res.redirect("/listings");
+}))
+
+app.all("*" ,(req,res,next)=>{
+    next(new ExpressError(404,"page not found!"))
 })
 
-
+app.use((err,req,res,next)=>{
+    // res.send("somting wrong")
+    let {statusCode=500,message="somting went wrong!"}=err;
+    res.status(statusCode).send(message)
+    
+})
 
 app.listen(8080,()=>{
     console.log('sever is listenning to port 8080');
 })
 
-app.use((err,req,res,next)=>{
-    res.send("somting wrong")
-})
+
 //Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 /* 
 app.get("/testListing",async(req,res)=>{
