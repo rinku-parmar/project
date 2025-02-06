f4faaea726b498264f718dd3354b86bdeb1ac406
rinku-parmar <rinkparmar022@gmail.com>
2025-02-06 20:08:23 +0530

    ------Login User------------------  GET /login [submit form]  POST /login [authenticate user exits or not --passport.authenticate() ]

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router=express.Router();
 const User=require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -20,6 +21,23 @@ router.post("/signup", wrapAsync(async(req,res)=>{
         res.redirect("/signup")
     }
    
-}))
+})
+);
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs")
+})
+
+router.post("/login",
+    passport.authenticate("local",{
+          failureRedirect:'/login' , 
+          failureFlash:true
+        }),
+         async(req,res)=>{
+         //res.send("Welcome to Wanderlust! You are logged in!");
+         req.flash("success","Welcome back to Wanderlust");
+         res.redirect("/listings");
+    }
+)
 
 module.exports=router;
